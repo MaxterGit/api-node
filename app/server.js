bfb40057ed144ddf6412b435478ca08606b00d29
Your Name <you@example.com>
2024-02-26 14:58:26 +0100

    que he cambiado

--- app/server.js
+++ app/server.js
@@ -1,7 +1,7 @@
 var express = require('express') //llamamos a Express
 var app = express()               
 
-var port = process.env.PORT || 8080  // establecemos nuestro puerto
+var port = process.env.PORT || 3000  // establecemos nuestro puerto
 
 app.get('/', function(req, res) {
   res.json({ mensaje: '¡Hola Mundo!' })   
